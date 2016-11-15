1da0cd069c63cd29f3b3a100602bc07e4271ad9c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-11-15 11:02:43 +0100

    Create step function

--- brainfuck.ml
+++ brainfuck.ml
@@ -33,3 +33,23 @@ let symbol command =
     | LoopStart        -> '['
     | LoopEnd          -> ']'
 
+type state = {
+  pointer: int
+}
+
+let start () =
+  { pointer = 0 }
+
+let point_at a_state pointer =
+  { a_state with pointer = pointer }
+
+let step a_state a_command =
+  match a_command with
+    | PointerIncrement -> { pointer = a_state.pointer + 1 }
+    | PointerDecrement -> { pointer = a_state.pointer - 1 }
+    | Increment        -> a_state
+    | Decrement        -> a_state
+    | Output           -> a_state
+    | Input            -> a_state
+    | LoopStart        -> a_state
+    | LoopEnd          -> a_state
--- brainfuck.mli
+++ brainfuck.mli
@@ -6,3 +6,9 @@ type command
 
 val command : char -> command option
 val symbol : command -> char
+
+type state
+
+val start: unit -> state
+val point_at: state -> int -> state
+val step: state -> command -> state
